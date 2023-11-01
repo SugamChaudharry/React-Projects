66c0aca0dec49123ab60cf55f4f35f761318b5cf
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-01 07:23:17 +0530

    custom react

--- 01_custom_react/CustomReact.js
+++ 01_custom_react/CustomReact.js
@@ -1,13 +1,4 @@
 function costomRender(reactElement, Container){
-    // not write way
-    /*const domElement = document.createElement(reactElement.type);
-    domElement.innerHTML = reactElement.childern;
-    domElement.setAttribute('href', reactElement.props.href);
-    domElement.setAttribute('target', reactElement.props.target);
-
-    Container.appendChild(domElement);*/
-
-    // using loop
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.childern;
     for (const prop in reactElement.props) {
