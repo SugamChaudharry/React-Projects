86fe4273d349be41d3af68f7f4a085a1e30a1d19
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-05 09:25:00 +0530

    theme project

---
+++ 08_themeSwitcher/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          {/* them btn*/}
+          <div className="w-full max-w-sm mx-auto">
+            { /* card*/}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
