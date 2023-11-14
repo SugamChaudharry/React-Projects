f109cfd1946deded44c0c44e426f3178c26874d4
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-14 19:16:39 +0530

    done dana done

--- 09_Todo-app/src/components/TodoList.jsx
+++ 09_Todo-app/src/components/TodoList.jsx
@@ -16,6 +16,9 @@ function TodoList({ todo }) {
     Todochecked(todo.id);
   };
 
+  const pathData =
+    "m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4";
+
   return (
     <div
       className={`  dark:bg-card-dark bg-card-light
@@ -26,28 +29,7 @@ function TodoList({ todo }) {
         className={`${isTodoEditable ? "hidden" : "flex"} `}
         id="checkBox"
         onClick={(e) => {
-          if (e.target.localName === "path") return;
-
           toggleCompleted();
-          const pathEl = `<path
-          d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4"
-          stroke=${theme === "dark" ? "white" : "black"} 
-          stroke-width="2" 
-          fill="none" 
-          class="path1"> </path>`;
-
-          e.target.childNodes[1].innerHTML = e.target.childNodes[1].innerHTML === "."  ? pathEl : ".";
-
-          const classis = `w-full h-[2rem] pr-[1.8rem] pl-[2.1rem] bg-transparent  border-b-[1px]
-              ${isTodoEditable ? "dark:border-deep-purple-400 border-black" : "border-none"}
-              placeholder:text-[#727575] text-[#484b6a]  text-lg focus:outline-none overflow-hidden line-through `;
-
-          const classis2 = `w-full h-[2rem] pr-[1.8rem] pl-[2.1rem] bg-transparent  border-b-[1px]
-              ${isTodoEditable ? "dark:border-deep-purple-400 border-black" : "border-none"}
-              placeholder:text-[#727575] text-[#484b6a] dark:text-[#cacde8]  text-lg focus:outline-none overflow-hidden `;
-
-          e.target.parentElement.nextSibling.className =
-            e.target.parentElement.nextSibling.className === classis ? classis2 : classis;
         }}>
         <svg width="45" height="45" viewBox="0 0 95 95">
           <rect
@@ -55,9 +37,19 @@ function TodoList({ todo }) {
             y="20"
             width="50"
             height="50"
-            stroke={`${theme === "dark" ? "white" : "black"}`}
-            fill="none"></rect>
-          <g transform="translate(0,-952.36222)">.</g>
+            stroke={theme === "dark" ? "white" : "black"}
+            fill="none"
+          />
+          <g transform="translate(0,-952.36222)">
+            {todo.completed && (
+              <path
+                d={pathData}
+                stroke={theme === "dark" ? "white" : "black"}
+                strokeWidth="2"
+                fill="none"
+              />
+            )}
+          </g>
         </svg>
       </div>
 
@@ -68,7 +60,9 @@ function TodoList({ todo }) {
         readOnly={!isTodoEditable}
         className={`w-full h-[2rem] pr-[1.8rem] pl-[2.1rem] bg-transparent  border-b-[1px] 
          ${isTodoEditable ? "dark:border-deep-purple-400 border-black" : "border-none"}
-        placeholder:text-[#727575] text-[#484b6a] dark:text-[#cacde8]  text-lg focus:outline-none overflow-hidden `}
+        placeholder:text-[#727575] text-[#484b6a] dark:text-[#cacde8]  text-lg focus:outline-none overflow-hidden ${
+          todo.completed && "line-through"
+        } `}
       />
 
       <div>
