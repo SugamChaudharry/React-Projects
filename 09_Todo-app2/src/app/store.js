7b0169fdbfdee8e116c11a47dd46af8f86afabca
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-20 05:57:14 +0530

    store setup

--- 09_Todo-app2/src/app/store.js
+++ 09_Todo-app2/src/app/store.js
@@ -3,5 +3,8 @@ import todoReducer from "../featuers/todo/todoSlice";
 import themeReducer from "../featuers/theme/themeSlice";
 
 export const store = configureStore({
-  reducer: todoReducer
+  reducer:{
+    todos: todoReducer,
+    theme: themeReducer,
+  }
 });
