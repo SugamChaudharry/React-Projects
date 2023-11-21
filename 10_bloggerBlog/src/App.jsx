4578714806bbe83aff2cbf41e40b0b9e5a27348c
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-21 06:19:31 +0530

    going for big brack

--- 10_bloggerBlog/src/App.jsx
+++ 10_bloggerBlog/src/App.jsx
@@ -1,4 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+
 function App() {
+  const [loding, setLoding] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then( (userData) => {})
+    .finally()
+  })
 
   console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
--- 10_bloggerBlog/src/store/Authslice.js
+++ 10_bloggerBlog/src/store/Authslice.js
@@ -5,13 +5,13 @@ const initialState = {
     userData : null,
 };
 
-const AuthSlice = createSlice({
+const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     login: (state, action) => {
       state.status = true;
-      state.userData = action.payload;
+      state.userData = action.payload.userData;
     },
     logout : (state) => {
         state.status = false;
@@ -20,8 +20,8 @@ const AuthSlice = createSlice({
   },
 });
 
-export const { login, logout } = AuthSlice.actions;
-export default AuthSlice.reducer;
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
 
 
 
