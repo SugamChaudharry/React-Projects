97da93774d34dda15d08796d324424e91aa7ccec
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-25 11:51:20 +0530

    logout btn

--- 10_bloggerBlog/src/appwrite/config.js
+++ 10_bloggerBlog/src/appwrite/config.js
@@ -1,4 +1,4 @@
-import config from "../config.js";
+import config from "../config/config.js";
 import { Client, ID , Databases , Query , Storage } from "appwrite";
 
 export class Service {
--- 10_bloggerBlog/src/components/index.js
+++ 10_bloggerBlog/src/components/index.js
@@ -1,4 +1,10 @@
 import Header from "./Header/header";
 import Footer from "./Footer/footer";
+import Container  from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./logo";
 
-export { Header, Footer };
+
+
+
+export { Header, Footer , Container , Logo , LogoutBtn};
