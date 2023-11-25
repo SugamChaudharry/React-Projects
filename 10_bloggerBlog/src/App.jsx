3fe4d7598d1fcd56431075a2a8b740e7632c4f46
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-25 08:11:13 +0530

    basic configtration complete

--- 10_bloggerBlog/src/App.jsx
+++ 10_bloggerBlog/src/App.jsx
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth';
-
+import { login , logout } from "./store/Authslice"
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 function App() {
   const [loding, setLoding] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then( (userData) => {})
-    .finally()
+    .then( (userData) => {
+      if(userData) {
+        dispatch(login({userData}));
+      }else {
+        dispatch(logout());
+      }
+    })
+    .finally( () => {setLoding(false)})
   })
-
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-    <h1>Bolg web</h1>
-    </>
-  )
+  return !loding ? (
+    <div className='bg-slate-900 w-full h-screen text-white flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          < Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
--- 10_bloggerBlog/src/appwrite/auth.js
+++ 10_bloggerBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import config from "../config.js";
+import config from "../config/config.js";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
