6d18d899cfb121f6b84d573cbba2eb04d6e58ae3
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-26 07:41:57 +0530

    input

--- 10_bloggerBlog/src/components/Input.jsx
+++ 10_bloggerBlog/src/components/Input.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useId } from 'react'
 
 const Input = React.forwardRef( function Input({
     label,
@@ -6,7 +7,19 @@ const Input = React.forwardRef( function Input({
     className = "",
     ...props
 } , ref) {
-    return <h1>Test</h1>
+    const id = useId();
+    return (
+      <div className="w-full">
+        {label && <label htmlFor={id}>{label}</label>}
+        <input
+            type={type}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            id={id}
+            {...props}
+        />
+      </div>
+    );
 } )  
 
 export default Input
